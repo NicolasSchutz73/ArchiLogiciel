a71152407b27bafd01c9f2d260c840e20a5afb55
Nicolas Schutz <90822955+NicolasSchutz73@users.noreply.github.com>
2024-10-07 09:11:37 +0200

    init

---
+++ src/main/java/com/example/ArchiLogi/ArchiLogiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ArchiLogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchiLogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchiLogiApplication.class, args);
+	}
+
+}
